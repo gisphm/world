7b45f22912334774b6dc45e73eb36019738b33e5
phm <phm8341@gmail.com>
2013-05-05 16:39:49 +0800

    phm

---
+++ world/AdmitBookmarkName.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace world
+{
+	public partial class 书签名称设置 : Form
+	{
+		public 书签名称设置()
+		{
+			InitializeComponent();
+		}
+	}
+}
